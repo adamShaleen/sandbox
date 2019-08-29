40e130eae604ef72fbfc96aab23c406cf954f0a0
adamShaleen <adamshaleen@gmail.com>
2019-08-29 14:24:34 -0500

    codewars word values challenge`

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,20 @@
+//---------------Code Wars Word Values------------------------------
+
+// this one was harder than it should have been
+
+// const alphaMap = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9,
+//   "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19,
+//   "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
+
+// function wordValue(arr) {
+//   return arr.map((str, i) => {
+//     const position = i + 1
+//     return str.replace(/[^a-z0-9]/g, "").split("").reduce((a, c) => {
+//       return a + alphaMap[c]
+//     }, 0) * position
+//   })  
+// }
+
 //---------------Code Wars My Languages-----------------------------
 
 // const myLanguages = (results) => {
